ab22d4ae16d133cf989102b74491a220274505bb
urosbmx <urosbmx@gmail.com>
2022-01-18 21:30:02 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Uroš Katanić on 18.1.22..
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
